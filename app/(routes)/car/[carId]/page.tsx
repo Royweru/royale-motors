42c8062a26453a93d9beed55576154917b1c5174
Royweru <royezi17@gmail.com>
2024-08-27 06:50:16 +0300

    deleted:    app/(routes)/[carId]/page.tsx 	modified:   app/(routes)/car/[carId]/page.tsx

--- app/(routes)/car/[carId]/page.tsx
+++ app/(routes)/car/[carId]/page.tsx
@@ -10,6 +10,14 @@ const CarIdPage = async ({ params }: { params: { carId: string } }) => {
   const cars = await fetchCars({});
 
   const car = cars.find((car) => car.id === params.carId);
+  if (!car)
+    return (
+      <div className=" w-full h-full items-center flex flex-col">
+        <h1 className=" h1 tracking-wide text-text-primary">
+          OOpsy there is no car!
+        </h1>
+      </div>
+    );
 
   return (
     <div className=" w-full h-full flex flex-col items-center justify-center">
