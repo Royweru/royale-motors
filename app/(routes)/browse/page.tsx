47c20b66fe7803c8097cf8a5ad6570b60be02ac9
Royweru <royezi17@gmail.com>
2024-10-05 13:16:27 +0300

    modified:   app/(routes)/browse/page.tsx

--- app/(routes)/browse/page.tsx
+++ app/(routes)/browse/page.tsx
@@ -1,7 +1,7 @@
 import { GetCars } from "@/actions/GetCars";
-import { Hero } from "@/components/hero";
+
 import React from "react";
-import { CheckFinance } from "./_components/check-finance";
+
 import { BodyFilter } from "./_components/body-filter";
 import { SearchHeader } from "./_components/search-header";
 import { CarDisplay } from "@/components/car-display";
@@ -9,7 +9,7 @@ import { Test } from "@/components/feature/test";
 import { GetModels } from "@/actions/GetModels";
 import { GetMakes } from "@/actions/GetMakes";
 import { GetTypes } from "@/actions/GetTypes";
-import { Braah_One } from "next/font/google";
+
 import { BrowseHero } from "@/components/hero/browse-hero";
 
 interface props {
